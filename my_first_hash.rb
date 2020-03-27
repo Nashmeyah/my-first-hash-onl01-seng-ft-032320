def00ffbb1ee2a4c15ef8ce97e07e0a1500af4ec
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-27 14:29:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"name" => "Nashmeyah", "age" => "24"}
-
-end
+# def my_hash
+#   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+#   my_hash = {"name" => "Nashmeyah", "age" => "24"}
+# 
+# end
 
 
 def shipping_manifest
