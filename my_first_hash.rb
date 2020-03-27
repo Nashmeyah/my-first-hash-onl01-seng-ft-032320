d57825f03da76981f9d90a329d04879ab4b38e41
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-27 10:54:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,9 +19,9 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+    "whale bone corsets" => "5",
+    "porcelain vases" => "2",
+    "oil paintings" => "3"
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
@@ -31,17 +31,17 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "whale bone corsets" => "5",
+                  "porcelain vases" => "2",
+                  "oil paintings" => "3"
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"] = 2
+  shipping_manifest["muskets"] = "2"
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest["gun powder"] = 4
+shipping_manifest["gun powder"] = "4"
 
   # return the shipping_manifest hash below
 
